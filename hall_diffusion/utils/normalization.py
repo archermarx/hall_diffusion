fix normalization issues with fourier vectors

hall_diffusion/utils/normalization.py
@@ -59,6 +59,10 @@ class Normalizer:
             norm = self.norm_tensor
         elif name in self.params():
             norm = self.norm_params
+        elif name in self.norm_fourier["names"]:
+            norm = self.norm_fourier
+        elif name in self.norm_perf["names"]:
+            norm = self.norm_perf
         else:
             raise KeyError(f"{name} is not a valid field or param in the given dataset.")
 
@@ -70,7 +74,7 @@ class Normalizer:
 
     def params(self) -> dict:
         return self.norm_params["names"]
-
+    
     def normalize(self, val, name: str):
         index, norm = self.find_name(name)
         mean, std, log = norm["mean"], norm["std"], norm["log"]
@@ -118,5 +122,26 @@ class Normalizer:
             denormed[:, i, :] = self.denormalize(tensor[:, i, :], name)
         return denormed
     
+    def extract_fourier_comps(self, vec):
+        if isinstance(vec, torch.Tensor):
+            vec = vec.clone()
+        elif isinstance(vec, np.ndarray):
+            vec = np.copy(vec)
+        mean = vec[0]
+        tens = vec[1:].reshape(-1, 3)
+        return mean, tens
+
+    def normalize_fourier(self, vec):
+        mean, tens = self.extract_fourier_comps(vec)
+        mean_norm = self.normalize(mean, "discharge_current_A")
+        tens[:, 0] = self.normalize(tens[:, 0], "frequency")
+        return np.concat(([mean_norm], tens.reshape((-1,))))
+
+    def denormalize_fourier(self, vec):
+        mean, tens = self.extract_fourier_comps(vec)
+        mean_denorm = self.denormalize(mean, "discharge_current_A")
+        tens[:, 0] = self.denormalize(tens[:, 0], "frequency")
+        return np.concat(([mean_denorm], tens.reshape((-1,))))
+
     def __eq__(self, other):
         return all(self.metadata_params.eq(other.metadata_params)) and all(self.metadata_tensor.eq(other.metadata_tensor))

hall_diffusion/utils/thruster_data.py
@@ -14,7 +14,8 @@ else:
     from .normalization import Normalizer
 
 def invert_fft_vector(t, fourier_vec):
-    fourier_vec = fourier_vec.numpy()
+    if isinstance(fourier_vec, torch.Tensor):
+        fourier_vec = fourier_vec.numpy()
     mean_current = fourier_vec[0]
     fourier_tensor = fourier_vec[1:].reshape((-1, 3))
     num_freqs = fourier_tensor.shape[0]
@@ -35,6 +36,29 @@ def invert_fft_vector(t, fourier_vec):
     signal = mean_current + np.sum(signal_mat, axis=0)
     assert signal.shape == (len(t),)
     return signal
+
+def compute_fft_vector(t, signal, num_freqs=None):
+    n = len(signal)
+    mean_current = np.mean(signal)
+    
+    # FFT of the signal (excluding DC component)
+    fft_coeffs = np.fft.rfft(signal) / n
+    freqs = np.fft.rfftfreq(n, d=(t[1] - t[0]))
+    
+    # Skip DC (index 0), take up to num_freqs
+    fft_coeffs = fft_coeffs[1:]
+    freqs = freqs[1:]
+    if num_freqs is not None:
+        fft_coeffs = fft_coeffs[:num_freqs]
+        freqs = freqs[:num_freqs]
+
+    # Normalize by mean, then extract real/imag
+    reals = fft_coeffs.real / mean_current  * 2 #for one-sided spectrum
+    imags = fft_coeffs.imag / mean_current * 2
+    
+
+    fourier_tensor = np.stack([freqs, reals, imags], axis=1)
+    return np.concatenate([[mean_current], fourier_tensor.flatten()])
 
 class ThrusterDataset(Dataset):
     def __init__(